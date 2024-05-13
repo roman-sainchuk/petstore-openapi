ae697be717ff08c4a8d9df5e4c47261c47efcb8f
Roman Sainchuk <albuman32@gmail.com>
2024-05-13 18:42:03 +0300

    Update test.js

--- g/test.js
+++ g/test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
-// 1, 2, 3, 4, 5, 6
+// 1, 2, 3, 4, 5, 6, 7
 describe('App', function() {
   it('has the default page', function(done) {
     request(app)
